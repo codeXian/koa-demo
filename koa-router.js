680d64e5f9fabb1c91508d1721cc4790ce0a287a
zhengxianfeng <codexian@yeah.net>
2018-01-30 14:59:10 +0800

    👀 add prefix

--- koa-router.js
+++ koa-router.js
@@ -2,7 +2,9 @@ const Koa = require('koa');
 const Router = require('koa-router');
 
 const app = new Koa();
-const router = new Router();
+const router = new Router({
+  prefix: '/codexian'
+});
 
 router
   .get('/', (ctx, next) => {
