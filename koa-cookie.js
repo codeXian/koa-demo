744a5a6c75e67e9adf5ef37fe00a08700096b0d0
zhengxianfeng <codexian@yeah.net>
2018-01-30 15:57:08 +0800

    🙀 koa-set-simple-cookie

---
+++ koa-cookie.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async(ctx) => {
+  if (ctx.url === '/index') {
+    ctx.cookies.set(
+      'MyName', 'codexian'
+    );
+    ctx.body = 'cookie is ok';
+  } else {
+    ctx.body = 'hello world'
+  }
+})
+
+app.listen(3000, () => console.log('server is on 3000 port'));
